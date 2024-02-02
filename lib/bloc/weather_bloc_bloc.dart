9ab7990c285e9c29e3cd3d901427447125d83b2b
victorzarzar <victorzarzar58@gmail.com>
2024-02-02 16:30:07 -0300

    updates in my data and weather bloc

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
 
 part 'weather_bloc_event.dart';
@@ -7,15 +8,16 @@ part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
+    on<WeatherBlocEvent>((event, emit) async {
       emit(WeatherBlocLoading());
       try {
         WeatherFactory wf = WeatherFactory("API_KEY", language: Language.ENGLISH);
+        Position position = await Geolocator.getCurrentPosition();
         Weather weather = await wf.currentWeatherByLocation(
-        latitude,
-        longitude
+        position.latitude,
+        position.altitude,
         );
-        emit(WeatherBlocSucess ());
+        emit(WeatherBlocSucess(weather));
       } catch (e) {
         emit(WeatherBlocFailure());
       }
